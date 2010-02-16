fd91914bcafae3ceaf782970b6c974eb33812d5b
Kevin Chapelier <kchapelier@deeo.fr>
2010-02-15 20:28:45 -0700

    Gone ahead, fixed the issues, added classes to the links and added support for @user/list.

--- Source/Tweetify.js
+++ Source/Tweetify.js
@@ -18,14 +18,19 @@ provides:
 */
 (function() {
 	var fn = function() {
-		return this.replace(/(https?:\/\/\S+)/gi,'<a href="$1">$1</a>').replace(/(^|\s)@(\w+)/g,'$1<a href="http://twitter.com/$2">@$2</a>').replace(/(^|\s)#(\w+)/g,'$1<a href="http://search.twitter.com/search?q=%23$2">#$2</a>');
+		return this.replace(/(https?:\/\/\S+)/gi,'<a href="$1">$1</a>')
+			.replace(/\B@(\w+)([^\/]\W)/g,'<a href="http://twitter.com/$1" class="twitterUser">@$1</a>$2') //users, but not lists
+			.replace(/\B@(\w+)\/(\w+)/g,'<a href="http://twitter.com/$1/$2" class="twitterList">@$1/$2</a>') //lists
+			.replace(/\B#(\w+)/g,'<a href="http://search.twitter.com/search?q=%23$1" class="twitterTag">#$1</a>'); //tags
 	};
+	
 	String.implement({
 		tweetify: fn
 	});
+	
 	Element.implement({
 		tweetify: function() {
-			this.set('text',fn.bind(this.get('text')).attempt());
+			this.set('html',(this.get('text')).tweetify());
 		}
 	});
 })();
