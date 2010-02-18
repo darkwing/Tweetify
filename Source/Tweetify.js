8510e59fbcb8f94dcf6bbfc5eb0b97fc578e7bab
David Walsh <davidwalsh83@gmail.com>
2010-02-17 19:00:59 -0600

    XHTML escaping per DEEO

--- Source/Tweetify.js
+++ Source/Tweetify.js
@@ -19,10 +19,12 @@ provides:
 */
 (function() {
 	var fn = function() {
-		return this.replace(/(https?:\/\/\S+)/gi, '<a href="$1">$1</a>')
-			.replace(/\B@(\w+)([^\/]\W)/g, '<a href="http://twitter.com/$1" class="twitterUser">@$1</a>$2') //users, but not lists
-			.replace(/\B@(\w+)\/(\w+)/g, '<a href="http://twitter.com/$1/$2" class="twitterList">@$1/$2</a>') //lists
-			.replace(/\B#(\w+)/g,'<a href="http://search.twitter.com/search?q=%23$1" class="twitterTag">#$1</a>'); //tags
+		return this.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;') //no more html injection
+					.replace(/(https?:\/\/\S+)/gi, '<a href="$1">$1</a>') //generic links
+					.replace(/\B@(\w+)([^\/]\W)/g, '<a href="http://twitter.com/$1" class="twitterUser">@$1</a>$2') //users, but not lists
+					.replace(/\B@(\w+)\/(\w+)/g, '<a href="http://twitter.com/$1/$2" class="twitterList">@$1/$2</a>') //lists
+					.replace(/\B#(\w+)/g,'<a href="http://search.twitter.com/search?q=%23$1" class="twitterTag">#$1</a>'); //tags
+		
 	};
 	
 	String.implement({
